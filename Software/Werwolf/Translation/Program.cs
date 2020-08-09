6aa46e191d7ef634d9e976fbc4b98b7620c96f2b
Akin <Semigroup@users.noreply.github.com>
2020-08-09 05:37:24 +0200

    high res

--- Software/Werwolf/Translation/Program.cs
+++ Software/Werwolf/Translation/Program.cs
@@ -7,12 +7,23 @@ using Werwolf.Forms;
 using Werwolf.Inhalt;
 using Translation.PDFWorkarounds;
 using Designer;
-
+using System.Runtime.InteropServices;
 
 namespace Translation
 {
     static class Program
     {
+        [DllImport("Shcore.dll")]
+        static extern int SetProcessDpiAwareness(int PROCESS_DPI_AWARENESS);
+
+        // According to https://msdn.microsoft.com/en-us/library/windows/desktop/dn280512(v=vs.85).aspx
+        private enum DpiAwareness
+        {
+            None = 0,
+            SystemAware = 1,
+            PerMonitorAware = 2
+        }
+
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
@@ -21,6 +32,10 @@ namespace Translation
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            SetProcessDpiAwareness((int)DpiAwareness.PerMonitorAware);
+
+
             Application.Run(new StartForm<Universe>(new TranslatingTool(),
                 new HintergrundTool()));//, new ProduktionSteik()
         }
--- Software/Werwolf/Werwolf/Program.cs
+++ Software/Werwolf/Werwolf/Program.cs
@@ -10,6 +10,7 @@ using Assistment.Extensions;
 using Assistment.Texts;
 using Assistment.Drawing.Geometries;
 using Assistment.Drawing.LinearAlgebra;
+using System.Runtime.InteropServices;
 //using Assistment.Drawing;
 
 using Werwolf.Generating;
@@ -21,11 +22,25 @@ namespace Werwolf
 {
     static class Program
     {
+        [DllImport("Shcore.dll")]
+        static extern int SetProcessDpiAwareness(int PROCESS_DPI_AWARENESS);
+
+        // According to https://msdn.microsoft.com/en-us/library/windows/desktop/dn280512(v=vs.85).aspx
+        private enum DpiAwareness
+        {
+            None = 0,
+            SystemAware = 1,
+            PerMonitorAware = 2
+        }
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            SetProcessDpiAwareness((int)DpiAwareness.PerMonitorAware);
+
             Application.Run(new StartForm<Universe>());
         }
     }
